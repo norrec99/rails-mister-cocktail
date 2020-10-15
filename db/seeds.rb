0cd36778fbdaf45f051dc920c47cae5dfde2dc4f
Cagatay Soyer <cagataysoyer@gmail.com>
2020-10-15 16:53:48 +0300

    controllers, models and views are created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+require "json"
+puts "Cleaning the database"
+Ingredient.destroy_all
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+dict = open(url).read
+
+the_cocktail_db = JSON.parse(dict)
+drinks = the_cocktail_db.values.flatten
+
+drinks.each do |name|
+  Ingredient.create(
+    name: name.values.first
+  )
+end
